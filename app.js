4e7eb5ee60a00b51153acf33a31b5b1106748012
justingracie <j.m.gracie@gmail.com>
2022-06-23 17:00:46 -0600

    got two simple sounds working

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
 console.log("hello world")
-
+window.addEventListener('keydown', function(e){
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
+    if(!audio) return;
+    audio.currentTime = 0;
+    audio.play()
+    console.log(audio)
+})
